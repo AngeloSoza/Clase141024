d18eb0405079c3798b11d28dc481c7f95277957c
Kenneth <kalacuna@uamv.edu.ni>
2024-10-14 08:27:48 -0600

    Class Program del ejercicio 2

---
+++ Ejercicio__2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Producto
+{
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+    public int Cantidad { get; set; }
+
+    public Producto(string nombre, double precio, int cantidad)
+    {
+        Nombre = nombre;
+        Precio = precio;
+        Cantidad = cantidad;
+    }
+}
