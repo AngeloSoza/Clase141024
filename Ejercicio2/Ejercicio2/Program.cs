f380349ad18e3812c5c9b4800dbb23f46108f9bb
Enrique Miguel Solis Sandoval <164054592+Enrique00007-uam@users.noreply.github.com>
2024-10-14 08:36:23 -0600

    Clase modificada

--- Ejercicio2/Ejercicio2/Program.cs
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -21,19 +21,8 @@ public class ControlDeInventario
 {
     using System;
 
-class Producto
-{
-    public string Nombre { get; set; }
-    public double Precio { get; set; }
-    public int Cantidad { get; set; }
 
-    public Producto(string nombre, double precio, int cantidad)
-    {
-        Nombre = nombre;
-        Precio = precio;
-        Cantidad = cantidad;
-    }
-}
+            
 
 class Inventario
 {
